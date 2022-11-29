b755c4057ba5f4e91f3fdfb5d31647d3203474ec
benjamin farmer <viroben2001@gmail.com>
2022-11-28 22:09:50 -0800

    part 6 done

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const methodOverride = require('method-override')
-
+const app = express()
 // Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
