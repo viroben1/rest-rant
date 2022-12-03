a3afec9bdd6882accb3dfa99c92ec5882e6f35d6
benjamin farmer <viroben2001@gmail.com>
2022-12-03 13:37:28 -0800

    got correct sytax in there

--- controllers/places.js
+++ controllers/places.js
@@ -3,13 +3,42 @@ const places = require('../models/places.js')
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id]})
+  }
+})
 //render place page
 router.get('/', (req, res) => {
   res.render('places/index', {places})
 })
+router.get('/:id', (req, res) => {
+  
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] ,id })
+  }
+})
+
+
+
 
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
+   let id = Number(req.params.id)
   if (isNaN(id)) {
       res.render('error404')
   }
